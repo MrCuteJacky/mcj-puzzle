b8d48c2fad11ae2b0de9d6273065162b5cf667aa
MrCuteJacky <jacky.tsai@promeritage.com.tw>
2019-01-25 09:30:05 +0800

    移除有版權的圖片

--- docs/js/puzzle.js
+++ docs/js/puzzle.js
@@ -1,16 +1,15 @@
 var puzzle;
-var space;
 var level = 0;
-var picture = [
-	['第一關(魯夫)', 'LV-1.jpg'],
-	['第二關(索隆)', 'LV-2.jpg'],
-	['第三關(娜美)', 'LV-3.jpg'],
-	['第四關(騙人布)', 'LV-4.jpg'],
-	['第五關(香吉士)', 'LV-5.jpg'],
-	['第六關(喬巴)', 'LV-6.jpg'],
-	['第七關(羅賓)', 'LV-7.jpg']
+var doRandom = false;
+
+var games = [
+	{title: 'MCJ', path: '01.png', size: 3},
+	{title: 'MCJ', path: '02.png', size: 4}
 ];
-var doRandom = true;
+var empty = {
+	x: games[0].size - 1,
+	y: games[0].size - 1
+}
 
 $(document).ready(function() {
 	makePuzzle();
@@ -22,47 +21,38 @@ $(document).ready(function() {
 			changePuzzle(window.event.keyCode);
 			showSelect();
 			draw();
-			if (checkWin()) {
-				setTimeout(() => {
-					level = level + 1 == picture.length ? 0 : level + 1;
-					makePuzzle();
-					showSelect();
-					alert('you win!');
-					draw();
-				}, 50);
-			}
+			checkWin();
 		}
 	});
 });
 
 function makePuzzle() {
+
 	var id = 1;
-	
-	puzzle = new Array(level + 4);
-	
-	for (var i = 0 ; i < level + 4 ; i++) {
-		puzzle[i] = new Array(level + 4);
-		for (var j = 0 ; j < level + 4 ; j++) {
+	puzzle = new Array(games[level].size);
+	for (var i = 0; i < puzzle.length; i++) {
+		puzzle[i] = new Array(games[level].size);
+		for (var j = 0; j < puzzle[i].length; j++) {
 			puzzle[i][j] = id++;
 		}
 	}
-	puzzle[level + 3][level + 3] = 0;
-	space = [level + 3, level + 3];
+	empty.x = games[level].size - 1;
+	empty.y = games[level].size - 1;
+	puzzle[empty.x][empty.y] = 0;
 
 	if (doRandom) {
-		for (var i = 0; i < level + 4; i++) {
-			for (var j = 0; j < level + 4; j++) {
-				if (i != level + 2 && j != level + 2) {
-					var x = Math.floor(Math.random() * (level + 2));
-					var y = Math.floor(Math.random() * (level + 2));
-					
-					var temp = puzzle[i][j];
-					
-					puzzle[i][j] = puzzle[x][y];
-					puzzle[x][y] = temp;
-					if (i == space[0] && j == space[1]) {
-						space = [x, y];
-					}
+		for (var i = 0; i < puzzle.length; i++) {
+			for (var j = 0; j < puzzle[i].length; j++) {
+				var x = Math.floor(Math.random() * (level + 2));
+				var y = Math.floor(Math.random() * (level + 2));
+				
+				var temp = puzzle[i][j];
+				
+				puzzle[i][j] = puzzle[x][y];
+				puzzle[x][y] = temp;
+				if (i == empty.x && j == empty.y) {
+					empty.x = x;
+					empty.y = y;
 				}
 			}
 		}
@@ -70,7 +60,7 @@ function makePuzzle() {
 }
 
 function showSelect() {
-	$('#level').html(picture[level][0]);
+	$('#title').html(games[level].title);
 }
 
 function checkWin() {
@@ -88,38 +78,47 @@ function checkWin() {
 			}
 		}
 	}
-	
-	return win;
+
+	if (win) {
+		setTimeout(() => {
+			level = level + 1 == games.length ? 0 : level + 1;
+			makePuzzle();
+			showSelect();
+			// alert('you win!');
+			$('#toast').toast('show');
+			draw();
+		}, 50);
+	}
 }
 
 function changePuzzle(keyCode) {
 	switch(keyCode) {
 	case 37:
-		if (space[1] + 1 < puzzle[space[0]].length) {
-			puzzle[space[0]][space[1]] = puzzle[space[0]][space[1] + 1];
-			puzzle[space[0]][space[1] + 1] = 0;
-			space = [space[0], space[1] + 1];
+		if (empty.y + 1 < puzzle[empty.x].length) {
+			puzzle[empty.x][empty.y] = puzzle[empty.x][empty.y + 1];
+			puzzle[empty.x][empty.y + 1] = 0;
+			empty.y++;
 		}
 		break;
 	case 38:
-		if (space[0] + 1 < puzzle.length) {
-			puzzle[space[0]][space[1]] = puzzle[space[0] + 1][space[1]];
-			puzzle[space[0] + 1][space[1]] = 0;
-			space = [space[0] + 1, space[1]];
+		if (empty.x + 1 < puzzle.length) {
+			puzzle[empty.x][empty.y] = puzzle[empty.x + 1][empty.y];
+			puzzle[empty.x + 1][empty.y] = 0;
+			empty.x++;
 		}
 		break;
 	case 39:
-		if (space[1] - 1 >= 0) {
-			puzzle[space[0]][space[1]] = puzzle[space[0]][space[1] - 1];
-			puzzle[space[0]][space[1] - 1] = 0;
-			space = [space[0], space[1] - 1];
+		if (empty.y - 1 >= 0) {
+			puzzle[empty.x][empty.y] = puzzle[empty.x][empty.y - 1];
+			puzzle[empty.x][empty.y - 1] = 0;
+			empty.y--;
 		}
 		break;
 	case 40:
-		if (space[0] - 1 < puzzle.length) {
-			puzzle[space[0]][space[1]] = puzzle[space[0] - 1][space[1]];
-			puzzle[space[0] - 1][space[1]] = 0;
-			space = [space[0] - 1, space[1]];
+		if (empty.x - 1 >= 0) {
+			puzzle[empty.x][empty.y] = puzzle[empty.x - 1][empty.y];
+			puzzle[empty.x - 1][empty.y] = 0;
+			empty.x--;
 		}
 		break;
 	}
@@ -127,17 +126,17 @@ function changePuzzle(keyCode) {
 
 function clickBlock(x, y) {
 	var changed = false;
-	if (space[0] == x) {
-		if (space[1] == y - 1 || space[1] == y + 1) {
-			puzzle[space[0]][space[1]] = puzzle[x][y];
-			space[1] = y;
+	if (empty.x == x) {
+		if (empty.y == y - 1 || empty.y == y + 1) {
+			puzzle[empty.x][empty.y] = puzzle[x][y];
+			empty.y = y;
 			puzzle[x][y] = 0;
 			changed = true;
 		}
-	} else if (space[1] == y) {
-		if (space[0] == x - 1 || space[0] == x + 1) {
-			puzzle[space[0]][space[1]] = puzzle[x][y];
-			space[0] = x;
+	} else if (empty.y == y) {
+		if (empty.x == x - 1 || empty.x == x + 1) {
+			puzzle[empty.x][empty.y] = puzzle[x][y];
+			empty.x = x;
 			puzzle[x][y] = 0;
 			changed = true;
 		}
@@ -145,15 +144,7 @@ function clickBlock(x, y) {
 	if (changed) {
 		showSelect();
 		draw();
-		if (checkWin()) {
-			setTimeout(() => {
-				level = level + 1 == picture.length ? 0 : level + 1;
-				makePuzzle();
-				showSelect();
-				alert('you win!');
-				draw();
-			}, 50);
-		}
+		checkWin();
 	}
 }
 
@@ -164,7 +155,7 @@ function draw() {
 	for (var i = 0 ; i < puzzle.length ; i++) {
 		html += '<tr>';
 		for (var j = 0 ; j < puzzle[i].length ; j++) {
-			html += '<td style="' + getStytle(level + 4, puzzle[i][j]) + '" onclick="clickBlock('+ i + ', ' + j + ')"></td>';
+			html += '<td style="' + getStytle(games[level].size, puzzle[i][j]) + '" onclick="clickBlock('+ i + ', ' + j + ')"></td>';
 		}
 		html += '</tr>';
 	}
@@ -184,16 +175,17 @@ function getStytle(size, id) {
 	var c = w / size;
 
 	var style = '';
+	var x = (id - 1) % puzzle.length;
+	var y = Math.floor((id - 1) / puzzle.length);
+	style += 'background-color: #FFFFFF;';
+	style += 'width: ' + c + 'px;';
+	style += 'height: ' + c + 'px;';
+	style += 'background-size: ' + (c * size) + 'px ' + (c * size) + 'px;';
+	style += 'background-position: -' + x * c + 'px -' + y * c + 'px;';
+	style += 'border: 5px solid white;';
+
 	if (id != 0) {
-		var x = (id - 1) % puzzle.length;
-		var y = Math.floor((id - 1) / puzzle.length);
-		style += 'background-color: #000000;';
-		style += 'width: ' + c + 'px;';
-		style += 'height: ' + c + 'px;';
-		style += 'background-image:URL(pic/' + picture[level][1] + ');';
-		style += 'background-size: ' + (c * size) + 'px ' + (c * size) + 'px;';
-		style += 'background-position: -' + x * c + 'px -' + y * c + 'px;';
-		style += 'border: 5px solid white;';
+		style += 'background-image:URL(images/' + games[level].path + ');';
 	}
 	
 	return style;
